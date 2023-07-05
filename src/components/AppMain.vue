b72223332719a183b17e47e311938c5170dcde44
marcocareddu <saldumaccu@tuta.io>
2023-07-05 16:32:23 +0200

    App AppMain - Ceil average vote

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,9 +16,10 @@ export default {
             <h4>{{ movie.title }}</h4>
             <h5>{{ movie.original_title }}</h5>
             <h5>{{ movie.original_language }}</h5>
-            <h5>{{ movie.vote_average }}</h5>
+            <h5>{{ Math.ceil(movie.vote_average) }}</h5>
         </li>
     </ul>
+
     <!-- Content Card HERE -->
     <AppCard />
 </template>
