9968f9766418d8d5758601a1a438ce8f4d9732db
marcocareddu <saldumaccu@tuta.io>
2023-07-05 16:10:53 +0200

    VUE App - Imported Azios, call API for movies

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script>
-import AppMain from './components/AppMain.vue'
-import AppHeader from './components/AppHeader.vue'
+import axios from 'axios';
+
+// Endpoint TMDB API
+const endpoint = 'https://api.themoviedb.org/3';
+const apiKey = '5446901a9478caa2f5783c5e24203b7c'
+
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
     data() {
         return {
+            movies: [],
         }
     },
     components: { AppMain, AppHeader, },
     methods: {
+
+        // Search movie title with API
         searchTitle(title) {
-            console.log(title)
+            axios.get(`${endpoint}/search/movie?api_key=${apiKey}&query=${title}`)
+                .then(res => {
+                    this.movies = res.data
+                })
         }
     }
 }
