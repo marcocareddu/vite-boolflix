62b24b66c7a5204431007933e21683c81f71afe0
marcocareddu <saldumaccu@tuta.io>
2023-07-05 15:38:19 +0200

    VUE AppSearch - Created search component

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+
+        return {
+            stringToSearch: '',
+        }
+    },
+
+    props: {
+        placeHolder: String,
+    },
+
+    emits: ['form-submit']
+}
+</script>
+
+<template>
+    <!-- Searchbar -->
+    <form @submit.prevent="$emit('form-submit', stringToSearch)">
+        <input type="text" :placeholder="placeHolder" v-model="stringToSearch">
+        <button>Cerca</button>
+    </form>
+</template>
+
+<style></style>
