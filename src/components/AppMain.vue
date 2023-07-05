7d677e9ef81871dc62e2562cc81839e7ab3bdf8e
marcocareddu <saldumaccu@tuta.io>
2023-07-05 16:29:58 +0200

    VUE AppMain - Display li for every filtered result

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,12 +2,23 @@
 import AppCard from './AppCard.vue'
 
 export default {
+    props: {
+        movies: Object,
+    },
     components: { AppCard }
 }
 
 </script>
 
 <template>
+    <ul>
+        <li v-for="movie in movies">
+            <h4>{{ movie.title }}</h4>
+            <h5>{{ movie.original_title }}</h5>
+            <h5>{{ movie.original_language }}</h5>
+            <h5>{{ movie.vote_average }}</h5>
+        </li>
+    </ul>
     <!-- Content Card HERE -->
     <AppCard />
 </template>
